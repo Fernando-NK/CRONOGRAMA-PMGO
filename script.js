ad2221c31600ace374a86dc0f1852dff2e41c022
Fernando-NK <fernandonogueirax21z42@gmail.com>
2026-04-24 12:11:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1740,7 +1740,7 @@ function composePreviewDay(dateKey, previousDay, runtime, carryItems = []) {
 
     sound.play().catch(() => {});
 
-    const targetVolume = 0.05;
+    const targetVolume = 0.10;
     const fadeInDuration = 140;
     const fadeSteps = 7;
     const stepTime = fadeInDuration / fadeSteps;
